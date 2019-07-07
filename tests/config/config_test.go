734051a27a0c2c178c189e2ddb9be9b779db9c73
Brendan Abolivier <contact@brendanabolivier.com>
2019-07-07 14:05:07 +0100

    Add new config options to the test config and parsing test

--- tests/config/config_test.go
+++ tests/config/config_test.go
@@ -20,6 +20,7 @@ func TestParseConfig(t *testing.T) {
 	tests.AssertEqual(t, cfg.Database.ConnString, ":memory:")
 
 	tests.AssertEqual(t, cfg.Ident.ServerName, "test")
+	tests.AssertEqual(t, cfg.Ident.BaseURL, "http://127.0.0.1:9999")
 
 	tests.AssertEqual(t, cfg.Ident.SigningKey.Algo, "ed25519")
 	tests.AssertEqual(t, cfg.Ident.SigningKey.ID, "0")
@@ -32,4 +33,15 @@ func TestParseConfig(t *testing.T) {
 	tests.AssertEqual(t, cfg.Ident.SigningKey.PrivKey, privKey)
 	tests.AssertEqual(t, cfg.Ident.SigningKey.PubKey, pubKey)
 	tests.AssertEqual(t, cfg.Ident.SigningKey.PubKeyBase64, pubKeyBase64)
+
+	tests.AssertEqual(t, cfg.Ident.Invites.SubjectTemplate, "{{.SenderDisplayName}} invited you to Matrix!")
+	tests.AssertEqual(t, cfg.Ident.Invites.EmailTemplate.Text, "templates/text/invite.txt")
+	tests.AssertEqual(t, cfg.Ident.Invites.EmailTemplate.HTML, "templates/html/invite.html")
+
+	tests.AssertEqual(t, cfg.Email.From, "Ident <ident@example.com>")
+	tests.AssertEqual(t, cfg.Email.SMTP.Hostname, "mail.example.com")
+	tests.AssertEqual(t, cfg.Email.SMTP.Port, "465")
+	tests.AssertEqual(t, cfg.Email.SMTP.Username, "ident@example.com")
+	tests.AssertEqual(t, cfg.Email.SMTP.Password, "somepassword")
+	tests.AssertEqual(t, cfg.Email.SMTP.EnableTLS, true)
 }
